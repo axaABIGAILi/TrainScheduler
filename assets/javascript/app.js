b8927268856e8441e1b9ce747e39e65f16ec88c2
Abi D <axa.abigail.i@gmail.com>
2019-07-09 19:08:47 -0500

    added minor css styling, linked firebase in JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyChakDxOtDwcVxrmBFr4pCI_9Lhnpi-pAQ",
+    authDomain: "train-scheduler-c2b0e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c2b0e.firebaseio.com",
+    projectId: "train-scheduler-c2b0e",
+    storageBucket: "",
+    messagingSenderId: "134811415158",
+    appId: "1:134811415158:web:ffb84f45866fc12d"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+var trainArray = [];
